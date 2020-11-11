76d491684c7f02860c6bafbcffa31771b8d8f9c9
victor.govorov <victorgovorov@mail.ru>
2020-11-11 17:15:02 +0500

    fixed comment

--- src/Formatting/ProtoBufMediaTypeFormatter.cs
+++ src/Formatting/ProtoBufMediaTypeFormatter.cs
@@ -39,7 +39,7 @@ namespace System.Net.Http.Formatting
         }
 
         /// <summary>
-        ///     >Provides protobuf serialization support for a number of types.
+        ///     Provides protobuf serialization support for a number of types.
         /// </summary>
         public TypeModel Model { get; }
 
